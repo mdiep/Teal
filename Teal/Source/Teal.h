ae2b6d4e6dcb46cde6ab10e6c8a2939124102a58
Matt Diephouse <matt@diephouse.com>
2018-10-19 08:22:40 -0400

    Create project

---
+++ Teal/Source/Teal.h
@@ -0,0 +1,7 @@
+@import UIKit;
+
+//! Project version number for Teal.
+FOUNDATION_EXPORT double TealVersionNumber;
+
+//! Project version string for Teal.
+FOUNDATION_EXPORT const unsigned char TealVersionString[];
---
+++ Teal/Tests/TealTests.swift
@@ -0,0 +1,7 @@
+import XCTest
+@testable import Teal
+
+class TealTests: XCTestCase {
+    func testExample() {
+    }
+}
